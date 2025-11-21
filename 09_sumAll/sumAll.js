56a5674059d4be170aeab4ce9a3cb92ae2db0486
madbark67 <239074869+madbark67@users.noreply.github.com>
2025-11-21 16:17:46 -0500

    09_sumAll: create sumAll() function to sum all values between two given numbers (inclusive)
    Implemented using a simple for loop to sum the given integers with if statement to pass test cases

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(floor, ceil) {
+    if (typeof(floor) != "number" || typeof(ceil) != "number" || !Number.isInteger(floor) || !Number.isInteger(ceil) || floor < 0 || ceil < 0) {
+        return "ERROR";
+    }
 
+    if (floor>ceil) {
+        let temp = ceil;
+        ceil = floor;
+        floor = temp;
+    }
+    let sumAns = 0;
+    for (let i = floor; i <= ceil; i++) {
+        sumAns = sumAns + i;
+    }
+    return sumAns;
 };
 
 // Do not edit below this line
