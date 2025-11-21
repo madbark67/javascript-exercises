b45e7f488d1fc82f96dcea90033b3867b6b8edbd
madbark67 <239074869+madbark67@users.noreply.github.com>
2025-11-21 15:59:41 -0500

    08_removeFromArray: Add removeFromArray() to remove all given params from array
    Used array.filter() and arrow function to remove items that were not a part of the given array, leveraged array.includes() to check

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+    let finalAns;
+    finalAns = arr.filter((arrItem) => {
+        return !num.includes(arrItem);
+    });
+    console.log(finalAns);
+    return finalAns;
 };
 
 // Do not edit below this line
