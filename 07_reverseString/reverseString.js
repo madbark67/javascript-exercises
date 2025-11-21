e4384516d738e0b4ea389b64c1b81fd9b27d233e
madbark67 <239074869+madbark67@users.noreply.github.com>
2025-11-21 15:39:31 -0500

    07_reverseString: implement reverseString function and pass test cases
    Used a for loop to iterate in reverse order over a string and return that to the user

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function (word) {
+    let newWord = "";
+    for (let i = word.length-1; i >= 0; i--) {
+        newWord = newWord.concat(word.charAt(i));
+    }
 
+    return newWord;
 };
 
 // Do not edit below this line
