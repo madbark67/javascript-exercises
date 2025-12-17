8ea703cc70c463aae30572b2de908247b54b8263
madbark67 <239074869+madbark67@users.noreply.github.com>
2025-12-16 21:12:55 -0500

    Complete 12_calculator exercise
    Completed calculator.js exercise using reduce()

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((sum, num) => sum += num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((mult, num) => mult *= num, 1);
 };
 
-const power = function() {
-	
+const power = function (base, expo) {
+  return base ** expo;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let fact = 1;
+  while (num > 1) {
+    fact = fact * num;
+    num--;
+  }
+  return fact;
 };
 
 // Do not edit below this line
